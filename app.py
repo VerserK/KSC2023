2e6759b4ee15b1dc92bd0ae4477c370347c81cee
akarawatp <akarawat.p@siamkubota.co.th>
2023-03-08 10:22:13 +0700

    tesaaaa

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from action import *
 from line import *
 from lib import *
 from tokenLine import *
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 
 app = Flask(__name__)
 CORS(app)
@@ -123,6 +124,27 @@ def appLine():
         return jsonify({ 'response':'OK'})
     except Exception as error:
         return jsonify({'response':'ER'})
+
+@app.route('/')
+def index():
+   print('Request for index page received')
+   return render_template('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
+
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = name)
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run()
