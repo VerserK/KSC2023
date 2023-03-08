9cfccfac8f4db228c15f3e18f0d07fc1655e4da2
akarawatp <akarawat.p@siamkubota.co.th>
2023-03-08 10:43:07 +0700

    ffffffffff

--- tokenLine.py
+++
@@ -1,5 +0,0 @@
-from lib import *
-
-def tokenLineBot():
-    dataTokenLine = "DyITlNB5yxdCVz0PON6HSVmND2UwXVnRF6mEzlE16R0k4f0+Tn9QCjYzt8cnYOnU67tdzaSzj0KXL2U9G/d8U1lW7qDOjOT0MSeAZ24YtinOXZYVEGyEwQjyUxY2h12WcNUIyTGQJ7GswwqJQZcofwdB04t89/1O/w1cDnyilFU="
-    return dataTokenLine
