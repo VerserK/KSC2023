2ce4710b1e9ee4c767fb94a78937af30bcdd6a58
akarawatp <akarawat.p@siamkubota.co.th>
2023-03-08 11:04:03 +0700

    aaaaaaa

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ from lib import *
 from config.token import *
 
 app = flask.Flask(__name__)
-CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
-app.config["DEBUG"] = True
 
 @app.route('/')
 def index():
