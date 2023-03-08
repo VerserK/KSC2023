dff64ec330ef120837ff27690f79655dda16b7d9
akarawatp <akarawat.p@siamkubota.co.th>
2023-03-08 11:26:03 +0700

    ascascasc

--- webhookLine/action.py
+++ webhookLine/action.py
@@ -2,17 +2,6 @@ import flask
 from flask import Flask ,session, render_template, request, redirect, url_for, send_from_directory
 from flask_cors import CORS, cross_origin
 from flask import request
-import psycopg2
-from psycopg2 import Error
-from typing import List, Tuple
-import jsons
-from flask import jsonify
-from jsonify import convert
-import uuid
-import requests
-import jwt
-import json
-import os
 from config.token import tokenLineBot
 from flexMessage.flex import selectTypeCar
 from flexMessage.flex import promotionFlex
