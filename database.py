cd609185d0b0f1976287a8a3faedaefb59f0dcf8
akarawatp <akarawat.p@siamkubota.co.th>
2023-05-30 13:55:23 +0700

    testqqqqq

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import pymssql
 
 def conn():
     try :
-        conn = pymssql.connect('localhost', 'sa', '702481007zz', "mtokm", 'utf8')
+        conn = pymssql.connect('skcdwhprdmi.siamkubota.co.th', 'DWH_CS', 'Skc@cs60281', "Service Data", 'utf8')
             
         return conn
     except Exception as error:
