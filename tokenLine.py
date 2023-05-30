8536606903c9b77e9a3cdb06119a0c2673348338
akarawatp <akarawat.p@siamkubota.co.th>
2023-05-30 11:22:49 +0700

    ีupdate token line

--- tokenLine.py
+++ tokenLine.py
@@ -1,3 +1,3 @@
 def tokenLineBot():
-    dataTokenLine = "DyITlNB5yxdCVz0PON6HSVmND2UwXVnRF6mEzlE16R0k4f0+Tn9QCjYzt8cnYOnU67tdzaSzj0KXL2U9G/d8U1lW7qDOjOT0MSeAZ24YtinOXZYVEGyEwQjyUxY2h12WcNUIyTGQJ7GswwqJQZcofwdB04t89/1O/w1cDnyilFU="
+    dataTokenLine = "16pXZt1NpuSNut2YWL7Y3punxBIkAsf1iGGJHg++eEO87jV0ippSRtL/O2d+wDBf7fZH4Td6g2I2nFLHc2LWQRaxO4SAckEiJsaufOFUBUUnve0ha8219upGnlQERFmjODG2XHeOZXjDiX/Pe51JbgdB04t89/1O/w1cDnyilFU="
     return dataTokenLine
